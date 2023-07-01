c3e78ad83cae70530d6109c5414dd8844c668528
soham <sohamlive17@gmail.com>
2023-07-01 20:52:44 +0530

    Redux

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+
+function Movies() {
+  const { data } = useGetMoviesQuery(); //fetching data from an API
+  console.log(data);
+
+  return (
+    <div>Movies</div>
+  );
+}
+export default Movies;
