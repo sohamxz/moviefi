895fdc122ac710e73d28768c0132c72a35dc6be0
soham <sohamlive17@gmail.com>
2023-07-02 02:56:04 +0530

    Genres and Categories

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,25 +4,22 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
 import useStyles from './styles';
+import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
 
 const lightLogo = 'https://fontmeme.com/permalink/230627/ec7d52f2b78ac37568a05991c0265367.png';
 const darkLogo = 'https://fontmeme.com/permalink/230627/20cc84d3636dd4eca67421becdd12430.png';
 
-const demoGenres = [
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-];
-const demoCategories = [
+const categories = [
   { label: 'Popular', value: 'popular' },
   { label: 'Top Rated', value: 'top_rated' },
   { label: 'Upcoming', value: 'upcoming' },
 ];
-
 function Sidebar({ setMobileOpen }) {
   const theme = useTheme();
   const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+  console.log(data);
 
   return (
     <>
@@ -30,21 +27,22 @@ function Sidebar({ setMobileOpen }) {
         <img
           className={classes.image}
           src={theme.palette.mode === 'light' ? lightLogo : darkLogo}
-          alt="Moviefi Site Logo"
+          alt="Filmpire Site Logo"
         />
       </Link>
       <Divider />
       <List>
         <ListSubheader>Categories</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
+        {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
             <ListItem
               onClick={() => {}} //will be implemented later
               button
             >
-              {/*<ListItemIcon>
-                <img src={...} className={classes.genreImages} height={30} />
-              </ListItemIcon>*/}
+
+              <ListItemIcon>
+                <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
+              </ListItemIcon>
               <ListItemText primary={label} />
             </ListItem>
           </Link>
@@ -53,22 +51,28 @@ function Sidebar({ setMobileOpen }) {
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
-        {demoGenres.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
-            <ListItem
-              onClick={() => {}} //will be implemented later
-              button
-            >
-              {/*<ListItemIcon>
-                <img src={...} className={classes.genreImages} height={30} />
-              </ListItemIcon>*/}
-              <ListItemText primary={label} />
-            </ListItem>
-          </Link>
-        ))}
+
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link key={name} className={classes.links} to="/">
+              <ListItem
+                onClick={() => {}} //will be implemented later
+                button
+              >
+                <ListItemIcon>
+                  <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
+            </Link>
+          ))
+        )}
       </List>
     </>
   );
 }
-
 export default Sidebar;
